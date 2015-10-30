5abdc963bca41aec401eaf75cb5338881996b382
Alex Bugosh <abugosh@gmail.com>
2015-10-30 17:16:18 -0500

    Adding acceptance tests for the artist-show

---
+++ tests/acceptance/artist-show-test.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import { make } from 'ember-data-factory-guy';
+import startApp from 'fe-ember-candidate/tests/helpers/start-app';
+
+var artist;
+module('Acceptance | artist show', {
+  beforeEach: function() {
+    this.application = startApp();
+    artist = make('artist');
+  },
+
+  afterEach: function() {
+    Ember.run(this.application, 'destroy');
+  }
+});
+
+test('visiting /artists/1', function(assert) {
+  visit('/artists/1');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/artists/1');
+  });
+});
+
+test('the artist page should have a header with the artist name', function(assert) {
+  visit('/artists/1');
+
+  andThen(function() {
+    assert.equal(find('h2.artist').text(), artist.get('name'));
+  });
+});
+
