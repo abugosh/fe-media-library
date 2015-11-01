1ee367046d219f86ee69daeb6efac0249dceab99
Alex Bugosh <abugosh@gmail.com>
2015-10-31 20:27:34 -0500

    Adding a route and links to album pages

--- app/router.js
+++ app/router.js
@@ -10,6 +10,10 @@ Router.map(function() {
   this.route('artists', function() {
     this.route('show', {path: '/:artist_id'});
   });
+
+  this.route('albums', function() {
+    this.route('show', {path: '/:album_id'});
+  });
 });
 
 export default Router;
